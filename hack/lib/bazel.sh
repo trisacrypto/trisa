2723fe386886fe286cdf6f3bb302cdbb19978cf4
skymeyer <jelle.vink@gmail.com>
2019-10-12 01:03:23 -0700

    Doc generation and initial CI setup

--- hack/lib/bazel.sh
+++ hack/lib/bazel.sh
@@ -4,8 +4,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+BAZEL_MODE=${BAZEL_MODE:-host}
+
 trisa::bazel::exec() {
-   bazel ${*}
+    if [ "${BAZEL_MODE}" = "docker" ]; then
+        trisa::bazel::docker::exec ${*}
+    else
+        bazel ${*}
+    fi
 }
 
 trisa::bazel::info::workspace() {
@@ -14,4 +20,8 @@ trisa::bazel::info::workspace() {
 
 trisa::bazel::info::bazel-bin() {
     printf $(trisa::bazel::exec info bazel-bin)
+}
+
+trisa::bazel::docker::exec() {
+    docker run --rm -v $(pwd):/workspace -w /workspace gcr.io/cloud-builders/bazel ${*}
 }
