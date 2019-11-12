766fc53f75a1ce27d1f27f2f86781ef8f56d4cd4
skymeyer <jelle.vink@gmail.com>
2019-11-11 18:00:17 -0800

    CI: switch dockerized bazel build to our own tooling

--- hack/lib/bazel.sh
+++ hack/lib/bazel.sh
@@ -4,15 +4,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-BAZEL_MODE=${BAZEL_MODE:-host}
 BAZEL_RC_FILE=${BAZEL_RC_FILE:-".bazelrc"}
 
 trisa::bazel::exec() {
-    if [ "${BAZEL_MODE}" = "docker" ]; then
-        trisa::bazel::docker::exec --bazelrc=${BAZEL_RC_FILE} ${*}
-    else
-        bazel --bazelrc=${BAZEL_RC_FILE} ${*}
-    fi
+    bazel --bazelrc=${BAZEL_RC_FILE} ${*}
 }
 
 trisa::bazel::info::workspace() {
@@ -21,12 +16,4 @@ trisa::bazel::info::workspace() {
 
 trisa::bazel::info::bazel-bin() {
     printf $(trisa::bazel::exec info bazel-bin)
-}
-
-trisa::bazel::docker::exec() {
-    docker run --rm \
-    -w /workspace \
-    -v $(pwd):/workspace \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    gcr.io/cloud-builders/bazel ${*}
 }
