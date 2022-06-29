446d08669311ba2499de15e063b950fccd9adeb7
nwhite222 <nwhite16@u.rochester.edu>
2022-06-29 11:06:48 -0400

    preliminary package doc

---
+++ pkg/doc.go
@@ -0,0 +1,12 @@
+// Copyright 2022 by the TRISA contributors. All rights reserved.
+// License that can be found in the LICENSE file.
+
+/* The pkg folder contains the reference implementation code, including compiled code generated from the protocol buffer definitions in the proto folder1.
+
+- The iso3166 folder contains language codes.
+- The ivms101 folder extends the generated protobuf code with JSON loading utilities, validation helpers, short constants, etc.
+- The trisa folder contains structs and methods for a range of TRISA-related tasks, such as performing cryptography and making mTLS connections.
+
+*/
+
+package pkg 
