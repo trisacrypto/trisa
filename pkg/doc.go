ecddfe4185d1c1b46cf031ae2c69f17c4006b700
nwhite222 <nwhite16@u.rochester.edu>
2022-06-30 11:09:51 -0400

    T2-sc-4374

--- pkg/doc.go
+++ pkg/doc.go
@@ -5,6 +5,7 @@
 
 	- The iso3166 folder contains language codes. Dictated by the International Organization for Standardization, these codes are an important part of TRISA's international compatibility. Language codes provide a consistent way to refer to countries across the world without the need for a lingua franca.
 	- The ivms101 folder extends the generated protobuf code with JSON loading utilities, validation helpers, short constants, etc. This package enables compatibility between international entities, making it easier to transmit descriptive information about individuals and organizations in a mutually intelligible way.
-	- The trisa folder contains structs and methods for a range of TRISA-related tasks, such as performing cryptography and making mTLS connections.
+	- The trisa folder contains structs and methods for a range of TRISA-related tasks, such as performing cryptography and making mTLS connections.status
+	
 */
 package pkg 
