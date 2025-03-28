925b6f2c2067bbb71c971397cc69bac9483f3b75
Benjamin Bengfort <benjamin@bengfort.com>
2025-03-28 11:06:30 -0500

    better parsing

--- pkg/slip0044/slip0044.go
+++ pkg/slip0044/slip0044.go
@@ -12,19 +12,22 @@ func (t CoinType) PathComponent() uint32 {
 
 // Return the coin symbol, which is preferred over the native String() method.
 func (t CoinType) Symbol() string {
-	return symbolsByType[t]
+	// Because the symbol is the first entry in the enum map, it is marked as the name
+	// in CoinType_name -- to ensure this happens, the slip0044.jsonl file must be
+	// ordered according to the correct symbol and the generate.py file in the proto
+	// folder must output the symbol as the first entry. This also depends on the way
+	// that protobuf generates enum.pb.go marking any duplicate values after the first
+	// value in the file.
+	return CoinType_name[int32(t)]
 }
 
 // Parse a coin from its symbol or name into the corresponding coin type.
 func ParseCoinType(coin string) (CoinType, error) {
-	// First try to parse as a symbol.
-	if coinType, ok := typesBySymbol[strings.ToUpper(coin)]; ok {
-		return coinType, nil
-	}
+	// Normalize the input to an enum-like value.
+	coin = strings.Replace(strings.ToUpper(strings.TrimSpace(coin)), " ", "_", -1)
 
-	// Otherwise try to parse as a name.
-	if coinType, ok := typesByName[strings.ToLower(coin)]; ok {
-		return coinType, nil
+	if ct, ok := CoinType_value[coin]; ok {
+		return CoinType(ct), nil
 	}
 
 	return 0, ErrUnknownCoin
